6059798cd40142349712016219d90bbdccb88dc1
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-05-20 16:26:46 -0500

    install axios, add request token function, edit typo on model import

--- controllers/spotify.js
+++ controllers/spotify.js
@@ -1,5 +1,6 @@
 const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env;
-const { SpotifyToken } = require("../models/spotifytoken");
+const { SpotifyToken } = require("../models/");
+const axios = require("axios")
 const randomstring = require("randomstring");
 
 const basicAuth =
@@ -24,15 +25,47 @@ const login = async (req, res) => {
   res.redirect(loginUrl);
 };
 
+const requestToken = (code, grant_type, token) => {
+    let data;
+    if (grant_type === "refresh_token") {
+      data = new URLSearchParams();
+      data.append('refresh_token', code);
+      data.append('grant_type', grant_type);
+    } else {
+      data = new URLSearchParams();
+      data.append('code', code);
+      data.append('grant_type', grant_type);
+      if (redirect_uri) {
+        data.append('redirect_uri', redirect_uri);
+      }
+    }  
+    return axios({
+        method: 'POST',
+        url: 'https://accounts.spotify.com/api/token', 
+        data,
+        headers: {
+          'Authorization': basicAuth,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(({ data }) => {
+        data.expires_in = new Date().getTime() + data.expires_in
+        token.update( data )
+        return token.save()
+      }).catch((error) => { return false })
+  }
+
 const jwt = async (req, res, next) => {
   req.token = await SpotifyToken.findOne({ where: {} });
-  if (!req.token && !req.query.code) {
-    return next();
+  if (!req.token && !req.query.code) { return next() }
+  if (!req.token && req.query.code) {
+    req.token = await requestToken(req.query.code, 'authorization_code', SpotifyToken.build({}))
+  } else if (now > req.token.expires_in) {
+    req.token = await requestToken(req.token.refresh_token, 'refresh_token', req.token)
   }
   if (!req.token) {
-    res.json({ error: "JWT could not be requested..." });
+    res.json({ error: 'JWT could not be requested...' })
   }
-  return next();
+  return next()
 };
 
 const auth = async (req, res) => {
